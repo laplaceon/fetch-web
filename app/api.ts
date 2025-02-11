b6286e0af91731f2808b73645355ea87fad86e59
Riyadh Rahman <8844262+laplaceon@users.noreply.github.com>
2025-02-11 02:03:53 -0500

    Added login form and prelim api method declarations

---
+++ app/api.ts
@@ -0,0 +1,78 @@
+const API_BASE_URL = "https://frontend-take-home-service.fetch.com";
+
+interface Dog {
+    id: string
+    img: string
+    name: string
+    age: number
+    zip_code: string
+    breed: string
+}
+
+interface Location {
+    zip_code: string
+    latitude: number
+    longitude: number
+    city: string
+    state: string
+    county: string
+}
+
+interface Coordinates {
+    lat: number;
+    lon: number;
+}
+
+export const login = async (name: string, email: string) => {
+    
+}
+
+export const logout = async () => {
+
+}
+
+export const getDogsByQuery = async (
+    size: number = 25,
+    sort?: string,
+    from?: number,
+    breeds?: string[], 
+    zipCodes?: string[], 
+    ageMin?: number, 
+    ageMax?: number 
+) => {
+}
+
+
+export const getDogsByIds = async (ids: number[]) => {
+}
+
+export const getDogsByMatch = async () => {
+    
+}
+
+export const getLocationsByZipcodes = async (zipCodes: string[]) => {
+
+}
+
+export const getLocationsByQuery = async (
+    size: number = 25,
+    from?: number,
+    city?: string,
+    states?: string[],
+    geoBoundingBox?: {
+        top: Location,
+        left: Location,
+        bottom: Location,
+        right: Location
+    } | {
+        bottom_left: Location,
+        top_right: Location
+    } | {
+        bottom_right: Location,
+        top_left: Location
+    }
+) => {
+
+}
+
+
